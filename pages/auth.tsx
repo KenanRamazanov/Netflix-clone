4df6f961f4ae1c2f11a23f5c60072b3901d6c097
kenan551 <kenan.ramazanov1104@gmail.com>
2024-01-24 21:13:05 +0400

    Editing auth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const auth = () => {
   return (
-    <div>auth</div>
+    <div
+    className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover "
+    >
+    <div className='bg-black h-full w-full bg-opacity-45'>
+
+    </div>
+    </div>
   )
 }
 
