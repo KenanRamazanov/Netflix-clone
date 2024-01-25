e277a9c3926ab4ebf6922a575ba06b0e9bae0599
kenan551 <kenan.ramazanov1104@gmail.com>
2024-01-25 23:36:24 +0400

    Final of Input

--- components/Input.tsx
+++ components/Input.tsx
@@ -36,7 +36,24 @@ const Input:React.FC<InputProps> = ({
       '
       placeholder='  '
     />
-    <label>
+    <label
+     className='absolute
+     text-base
+     scale-75
+     -translate-y-1
+     z-10
+     left-7
+    text-zinc-400
+    peer-placeholder-shown:scale-100
+    peer-placeholder-shown:translate-y-0
+    peer-focus:scale-75
+    peer-focus:-translate-y-2
+    duration-150
+    origin-[0]
+    transform
+    top-3'
+    htmlFor={id}
+    >
         {label}
     </label>
     </div>
