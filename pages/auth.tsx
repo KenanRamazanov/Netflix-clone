8cad5e2146fe5025888247d3ef39dc85e0ed8fad
kenan551 <kenan.ramazanov1104@gmail.com>
2024-01-25 22:52:44 +0400

    Adding logo

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -3,10 +3,14 @@ import React from 'react'
 const auth = () => {
   return (
     <div
-    className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover "
+   className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover "
     >
-    <div className='bg-black h-full w-full bg-opacity-45'>
-
+    <div
+     className='bg-black h-full w-full bg-opacity-45'
+     >
+   <nav className='px-12 py-6'>
+   <img src="/images/logo.png" className='h-12'/>
+   </nav>
     </div>
     </div>
   )
