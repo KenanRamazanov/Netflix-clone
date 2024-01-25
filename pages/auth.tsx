36b6932efe29b25d8be87596cf9e622461449404
kenan551 <kenan.ramazanov1104@gmail.com>
2024-01-25 23:27:28 +0400

    Creating Input

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -11,6 +11,18 @@ const auth = () => {
    <nav className='px-12 py-6'>
    <img src="/images/logo.png" className='h-12'/>
    </nav>
+   
+   <div className='flex justify-center'>
+       <div className='bg-black bg-opacity-85 px-20 py-20 self-center mt-2 lg:w-2/5 rounded-xl w-full'>
+        <h2 className='text-white text-5xl mb-8 font-semibold'>
+          Register
+        </h2>
+        <div className='flex flex-col gap-4'>
+         
+        </div>
+       </div>
+   </div>
+
     </div>
     </div>
   )
