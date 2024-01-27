a5e9b1b97f87e5fc5ddc3c7b8708063df1c70184
kenan551 <kenan.ramazanov1104@gmail.com>
2024-01-28 00:37:22 +0400

    Fixing all errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
+import { Metadata } from "next";
 import { Inter } from "next/font/google";
+
 
 const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -1,6 +1,31 @@
+import { NextPageContext } from 'next';
+import { getSession } from 'next-auth/react';
 import React from 'react'
 
+
+export async function getServerSideProps(context: NextPageContext) {
+
+  const session = await getSession(context);
+
+  if(!session){
+    return{
+      redirect:{
+        destination:'/auth',
+        permanent : false,
+      }
+    }
+  }
+
+  return{
+    props: {}
+  }
+  
+}
+
+
 const profiles = () => {
+
+  
   return (
     <div>profiles</div>
   )
