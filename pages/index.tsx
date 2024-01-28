69783d9d4e34926ab6a18fb8af083aad04c45d9c
kenan551 <kenan.ramazanov1104@gmail.com>
2024-01-29 00:46:33 +0400

    Adding user to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,33 @@
-import { Metadata } from "next";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { Metadata, NextPageContext } from "next";
+import { getSession } from "next-auth/react";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
+export async function getServerSideProps(context: NextPageContext) {
+
+  const session = await getSession(context);
+
+  if(!session){
+    return{
+      redirect:{
+        destination:'/auth',
+        permanent : false,
+      }
+    }
+  }
+
+  return{
+    props: {}
+  }
+  
+}
 export default function Home() {
+
+const {data :user} = useCurrentUser();
   return (
     <>
+    <h1>Hello index</h1>
     </>
   );
 }
