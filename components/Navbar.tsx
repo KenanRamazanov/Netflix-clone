2c6f5a4c0daadbdad4de166c2dd9152c15c0770d
kenan551 <kenan.ramazanov1104@gmail.com>
2024-01-29 23:29:59 +0400

    Adding NavItem to Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { BellIcon, ChevronDownIcon, MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+import NavItem from './NavItem'
 const Navbar = () => {
   return (
    <nav className='w-full fixed z-20'>
     <div className='px-4 py-6 flex flex-row transition'>
       <img src="/images/logo.png" className='lg:h-8 h-6' />
       <div className='flex-row lg:flex hidden  gap-7 ml-12'>
-                 
+         <NavItem name='Home' active></NavItem>
+         <NavItem name='Films' ></NavItem>
+         <NavItem name='Series' ></NavItem>
+         <NavItem name='New & Popular' ></NavItem>
+         <NavItem name='My List' ></NavItem>
+        <NavItem name='Browse My Languages' ></NavItem>
+
          </div>
     </div>
    </nav>
