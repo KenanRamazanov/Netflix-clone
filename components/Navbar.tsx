a595273b38066ae555018b243d3238a09bbd27f2
kenan551 <kenan.ramazanov1104@gmail.com>
2024-01-29 23:26:12 +0400

    Adding Logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import { BellIcon, ChevronDownIcon, MagnifyingGlassIcon } from '@heroicons/react/24/solid'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+   <nav className='w-full fixed z-20'>
+    <div className='px-4 py-6'>
+      <img src="/images/logo.png" alt="" />
+    </div>
+   </nav>
   )
 }
 
