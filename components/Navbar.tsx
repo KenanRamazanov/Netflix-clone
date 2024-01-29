d0d2d2cd667f53e5d06bf5a272dd253cdbde252f
kenan551 <kenan.ramazanov1104@gmail.com>
2024-01-29 23:28:47 +0400

    Creating NavItem

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,8 +3,11 @@ import { BellIcon, ChevronDownIcon, MagnifyingGlassIcon } from '@heroicons/react
 const Navbar = () => {
   return (
    <nav className='w-full fixed z-20'>
-    <div className='px-4 py-6'>
-      <img src="/images/logo.png" alt="" />
+    <div className='px-4 py-6 flex flex-row transition'>
+      <img src="/images/logo.png" className='lg:h-8 h-6' />
+      <div className='flex-row lg:flex hidden  gap-7 ml-12'>
+                 
+         </div>
     </div>
    </nav>
   )
