125a000cf9d822ce8ede44c2f03aa38739fa6574
kenan551 <kenan.ramazanov1104@gmail.com>
2024-01-29 23:19:13 +0400

    Adding User's mail to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,11 +23,17 @@ export async function getServerSideProps(context: NextPageContext) {
   
 }
 export default function Home() {
-
 const {data :user} = useCurrentUser();
   return (
     <>
-    <h1>Hello index</h1>
+    <h1 className="text-4xl bg-red-500 font-bolc underline">
+      Hello index
+      
+      </h1>
+      <p>
+      {user?.email}
+      </p>
+      <button className="w-full bg-black text-white text-xl">Logout</button>
     </>
   );
 }
