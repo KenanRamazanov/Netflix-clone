7332905aeccbfbb6a742ba17913bdbe48d2a3061
kenan551 <kenan.ramazanov1104@gmail.com>
2024-01-30 11:35:02 +0400

    Final of MobileItem

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
-
-const MobileMenu = () => {
+interface MobileMenuProps {
+    visible?: boolean;
+}
+const MobileMenu:React.FC<MobileMenuProps> = ({visible}) => {
+    if(!visible) {
+        return null;
+    }
   return (
-    <div>MobileMenu</div>
+    <div className='bg-gray-950 w-48 absolute rounded-lg top-8 border-2 left-0 py-5 flex-col border-gray-800 flex'>
+    MobileMenu
+
+    <div className='flex flex-col gap-4'>
+     <div className='text-white px-3 text-center hover:underline'>
+        Home
+     </div>
+     <div className='text-white px-3 text-center hover:underline'>
+        Film
+     </div> 
+    <div className='text-white px-3 text-center hover:underline'>
+        New & Popular
+     </div> 
+    <div className='text-white px-3 text-center hover:underline'>
+     My List
+     </div> 
+    <div className='text-white px-3 text-center hover:underline'>
+     Browse by Langugaes
+     </div>
+    </div>
+    </div>
   )
 }
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { BellIcon, ChevronDownIcon, MagnifyingGlassIcon } from '@heroicons/react/24/solid'
 import NavItem from './NavItem'
+import MobileMenu from './MobileMenu'
 const Navbar = () => {
+  const [MobileMenuItem,setMobileMenu] = useState(false)
+  const toogleMobileMenu = useCallback(() => {
+    setMobileMenu((current) => !current)
+},[])
   return (
    <nav className='w-full fixed z-20'>
     <div className='px-4 py-6 flex flex-row transition'>
@@ -14,9 +19,10 @@ const Navbar = () => {
          <NavItem name='My List' ></NavItem>
         <NavItem name='Browse My Languages' ></NavItem>
          </div>
-         <div className='relative lg:hidden flex flex-row items-center gap-2 ml-6'>
+         <div onClick={toogleMobileMenu} className='relative lg:hidden flex flex-row items-center gap-2 ml-6'>
          <p className='text-white'>Browse</p>
-         <ChevronDownIcon className='text-white w-5'/>
+         <ChevronDownIcon className='text-white w-5 cursor-pointer'/>
+         <MobileMenu visible={MobileMenuItem} />
          </div>
     </div>
    </nav>
