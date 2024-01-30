f1e833e4799a6fbff77eb4f143f2d5e83f902fb3
kenan551 <kenan.ramazanov1104@gmail.com>
2024-01-30 11:37:01 +0400

    Improve Css in MobileMenu

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -8,22 +8,20 @@ const MobileMenu:React.FC<MobileMenuProps> = ({visible}) => {
     }
   return (
     <div className='bg-gray-950 w-48 absolute rounded-lg top-8 border-2 left-0 py-5 flex-col border-gray-800 flex'>
-    MobileMenu
-
     <div className='flex flex-col gap-4'>
-     <div className='text-white px-3 text-center hover:underline'>
+     <div className='text-white px-3 text-center hover:underline cursor-pointer'>
         Home
      </div>
-     <div className='text-white px-3 text-center hover:underline'>
+     <div className='text-white px-3 text-center hover:underline cursor-pointer'>
         Film
      </div> 
-    <div className='text-white px-3 text-center hover:underline'>
+    <div className='text-white px-3 text-center hover:underline cursor-pointer'>
         New & Popular
      </div> 
-    <div className='text-white px-3 text-center hover:underline'>
+    <div className='text-white px-3 text-center hover:underline cursor-pointer'>
      My List
      </div> 
-    <div className='text-white px-3 text-center hover:underline'>
+    <div className='text-white px-3 text-center hover:underline cursor-pointer'>
      Browse by Langugaes
      </div>
     </div>
