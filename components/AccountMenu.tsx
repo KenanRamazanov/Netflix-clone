6c5bf0c4601d020dffefd9dcdaf65da09c2b6462
kenan551 <kenan.ramazanov1104@gmail.com>
2024-01-31 20:24:14 +0400

    Final of AccountMenu

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -14,8 +14,19 @@ const AccountMenu: React.FC<AccountProp>=({visible})=> {
     }
 
   return (
-    <div>
-    AccountMenu
+    <div className='bg-gray-950 w-48 absolute rounded-lg top-8 
+    border-2 right-0 py-5 flex-col border-gray-800 flex'>
+    <div className='flex flex-col gap-3'>
+      <div className='px-3  flex flex-row gap-3 items-center w-full'>
+      <img className='w-6' src='/images/default-red.png'></img>
+         <p className=' cursor-pointer  text-white text-lg hover:underline'>{user?.name}</p>
+      </div>
+      <hr className='bg-gray-500 border-0 h-px my-4' />
+      <div onClick={()=>{}} className=' cursor-pointer px-3 text-white text-center hover:underline'>
+                Sign out Netflix
+            </div>
+
+    </div>
     </div>
   )
 }
