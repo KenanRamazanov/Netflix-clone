1473d944f2dad65ece4edde0ff750c121e170412
kenan551 <kenan.ramazanov1104@gmail.com>
2024-01-31 21:19:05 +0400

    Creating Movie api

---
+++ pages/api/movie/index.ts
@@ -0,0 +1,30 @@
+import serverAuth from "@/libs/serverAuth";
+import { NextApiRequest, NextApiResponse } from "next";
+import prismadb from '@/libs/prismadb'
+
+
+export default async function handler (req : NextApiRequest, res: NextApiResponse){
+
+    try{
+        if(req.method !== 'GET')
+        {
+            return res.status(405).end();
+        }
+
+        await serverAuth(req, res);
+
+        const MovieList = await prismadb.movie.findMany();
+
+       
+
+        return res.status(200).json(MovieList);
+       
+    
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).end();
+    }
+
+
+}
