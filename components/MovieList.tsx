242d4e3639564a9cd07c5b71ecbc2912a913d8d7
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-01 20:31:37 +0400

    Creating MovieCard

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -18,6 +18,11 @@ const  MovieList: React.FC<MovieListProps>=({data,title})=> {
           <p  className='text-white text-md md:text-xl lg:text-2xl font-semibold mt-5 mb-5'>
           {title}
           </p>
+          <div className='grid grid-cols-3 gap-2'>
+           {data.map((movie) => (
+            <p>{movie.title}</p>
+           ))}
+          </div>
         </div>
     </div>
   )
