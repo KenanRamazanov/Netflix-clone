cbb3de4e4ae0297a367dc52658f2d66ad8a09b9b
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-02 20:03:27 +0400

    Adding images

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,13 +1,26 @@
 import { MovieInterface } from '@/types';
-import React from 'react'
+import { useRouter } from 'next/router';
+import React, { useCallback } from 'react'
 
 interface MovieCartProp{
     data : MovieInterface;
 }
 
 const  MovieCard: React.FC<MovieCartProp>=({data})=> {
+    const router = useRouter();
+
+//   const {openModal} = useInfoModalStore();
+
+  const redirectToWatch  = useCallback(()=> router.push(`/watch/${data.id}`), [router, data.id] )
   return (
-    <div>MovieCard</div>
+    <div className='group bg-zinc-800 col-span-1 relative h-52'>
+       <img onClick={redirectToWatch} src={data.thumbnailUrl} 
+        className='h-52 
+        w-full object-cover
+        shadow-xl rounded-lg
+        group-hover:opacity-70
+         cursor-pointer'/>
+    </div>
   )
 }
 
