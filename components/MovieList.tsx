b263180a8b53b9f19ef836c8260dda09cd9b9206
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-03 18:08:22 +0400

    Adding Swiper to MovieList

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
-import { isEmpty} from  'lodash';
-import { MovieInterface } from '@/types';
+import { isEmpty } from 'lodash';
 import MovieCard from './MovieCard';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { MovieInterface } from '@/types';
+
 interface MovieListProps {
 
     data:MovieInterface[];
@@ -15,16 +18,45 @@ const  MovieList: React.FC<MovieListProps>=({data,title})=> {
     }
   return (
     <div className='px-4 space-y-6'>
-        <div>
-          <p  className='text-white text-md md:text-xl lg:text-2xl font-semibold mt-5 mb-5'>
-          {title}
-          </p>
-          <div className='grid grid-cols-3 gap-2'>
-           {data.map((movie) => (
-             <MovieCard data={movie} key={movie.id}></MovieCard>
-           ))}
-          </div>
+      <div>
+        <p 
+        className='text-white text-md md:text-xl lg:text-2xl font-semibold mt-5 mb-5'
+        >{title}</p>
+        <div className='gap-2'>
+        <Swiper watchSlidesProgress={true} 
+         slidesPerView={2}
+         spaceBetween={10}
+      
+         breakpoints={{
+           640: {
+             slidesPerView: 3,
+             spaceBetween: 10,
+           },
+           768: {
+             slidesPerView: 4,
+             spaceBetween: 20,
+           },
+           1024: {
+             slidesPerView: 6,
+             spaceBetween: 20,
+           },
+         }}
+        className="h-80 ">
+
+         {data.map((movie)=>(
+            <SwiperSlide className='hover:scale-[5.7]'>
+         <MovieCard data={movie} key={movie.id}></MovieCard>
+         </SwiperSlide>
+         ))}
+
+</Swiper>
+
+
         </div>
+      </div>
+
+
+
     </div>
   )
 }
