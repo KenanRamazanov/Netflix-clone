b9af5821bb76cfddc8122357aa1743331e0213fb
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-04 19:59:06 +0400

    Final of FavoriButton

--- components/FavoriButton.tsx
+++ components/FavoriButton.tsx
@@ -2,7 +2,7 @@ import useCurrentUser from '@/hooks/useCurrentUser';
 import useFavoriMovie from '@/hooks/useFavorites';
 import axios from 'axios';
 import React, { useCallback, useMemo, useState } from 'react'
-
+import { PlusIcon, CheckIcon } from '@heroicons/react/24/solid'
 interface favoriButtonProps{
     movieId :string;
 }
@@ -41,11 +41,19 @@ const FavoriButton: React.FC<favoriButtonProps>=({movieId})=> {
 
         }
     },[movieId, isFavorite, MuteFavori])
-
+    const Icon = isFavorite ? CheckIcon : PlusIcon;
 
    
   return (
-    <div>FavoriButton</div>
+    <div onClick={toogleFavorites} 
+    className='cursor-pointer border-white rounded-full flex
+    items-center justify-center border-2 w-7 h-7 lg:w-11 lg:h-11
+    hover:border-neutral-400
+    '>
+        <Icon className='text-white w-4 h-4 lg:w-8 lg:h-8 '></Icon>
+
+
+    </div>
   )
 }
 
