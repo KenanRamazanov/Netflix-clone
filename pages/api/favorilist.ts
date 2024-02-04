2b082d26554e85e56e984881228b8130f72850c4
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-04 19:40:34 +0400

    Creating favoritelist api

---
+++ pages/api/favorilist.ts
@@ -0,0 +1,36 @@
+import serverAuth from "@/libs/serverAuth";
+import { NextApiRequest, NextApiResponse } from "next";
+import prismadb from '@/libs/prismadb'
+
+
+export default async function handler (req : NextApiRequest, res: NextApiResponse){
+
+    try{
+        if(req.method !== 'GET')
+        {
+            return res.status(405).end();
+        }
+
+         const  {currentUser} =  await serverAuth(req, res);
+
+         const favoriMovie = await prismadb.movie.findMany({
+            where : {
+                id: {
+                    in:currentUser?.favoriteIds,
+                }
+            }
+         })
+
+         return res.status(200).json(favoriMovie);
+
+       
+       
+    
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).end();
+    }
+
+
+}
