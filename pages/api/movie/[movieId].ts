b49837381fe977e517c6f40449af5158f3b32198
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-04 20:26:54 +0400

    Creating movieId

---
+++ pages/api/movie/[movieId].ts
@@ -0,0 +1,44 @@
+import serverAuth from "@/libs/serverAuth";
+import { NextApiRequest, NextApiResponse } from "next";
+import prismadb from '@/libs/prismadb'
+
+
+export default async function handler (req : NextApiRequest, res: NextApiResponse){
+
+    try{
+        if(req.method !== 'GET')
+        {
+            return res.status(405).end();
+        }
+
+        await serverAuth(req, res);
+
+
+        const {movieId} = req.query;
+
+        if(typeof movieId !== 'string'){
+            throw new Error('Invalid id')
+        }
+
+        if(!movieId){
+            throw new Error('Missing id')
+        }
+
+        const movies = await prismadb.movie.findUnique({
+            where : {
+                id:movieId
+            }
+        });
+
+
+        return res.status(200).json(movies);
+       
+    
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).end();
+    }
+
+
+}
